8adc253acc6bf7a8fbb0e44d1353bbcb33821ee0
Mohammad <mohammadmghig@gmail.com>
2023-06-29 01:56:22 +0330

    init

--- main.go
+++ main.go
@@ -1,18 +1,79 @@
 package main
 
- 
-
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	//readProccess()
-	cpuUtilization()
+
+	flagString := flag.String("k", "", "Specify a string flag")
+
+	flag.Parse()
+
+	if(*flagString == "process"){
+		readProccess()
+		return
+	}
+	
+	if *flagString == "cpu"{
+		cpuUtilization()
+		return
+	}
+
+	
+
+
+	fmt.Printf("Please set a flag like k=cpu for checking cpu utilization ...");
+
+}
+
+func cpuUtilization() {
+
+
+	// Create a ticker that ticks every second
+	ticker := time.NewTicker(1 * time.Second)
+
+	go func() {
+
+		for range ticker.C{
+			// Get the initial CPU usage
+			prevTime := time.Now()
+			prevCPU := getCPUUsage()
+
+			// Wait for a specific duration
+			time.Sleep(1 * time.Second)
+
+			// Get the updated CPU usage
+			currTime := time.Now()
+			currCPU := getCPUUsage()
+
+			// Calculate the CPU utilization percentage
+			totalTime := currTime.Sub(prevTime).Seconds()
+			cpuUtilization := 100 * (float64(currCPU-prevCPU) / float64(runtime.NumCPU()) / totalTime)
+
+			fmt.Printf("CPU Utilization: %.2f%%\n", cpuUtilization)
+		}
+	
+	}()	
+
+	select {}
+
+ 
+
+}
+
+// Returns the current CPU time as reported by the Go runtime
+func getCPUUsage() int64 {
+	var s runtime.MemStats
+	runtime.ReadMemStats(&s)
+	return int64(s.Sys)
 }
 
 
