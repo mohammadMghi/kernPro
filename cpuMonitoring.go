6ea270b78301614a10e977f690cb7f5a209c04fc
Mohammad <mohammadmghig@gmail.com>
2023-06-29 01:40:34 +0330

    init

--- cpuMonitoring.go
+++ cpuMonitoring.go
@@ -1,4 +1,4 @@
-package kernpro
+package main
 
 
 import (
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package kernpro
+package main
 
  
 
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	readProccess()
+	//readProccess()
 	cpuUtilization()
 }
 
